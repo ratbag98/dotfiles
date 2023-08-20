d3f1c456fb87cf67888cb766eb34a3c36f0ece88
Rob Rainthorpe <robr@robrainthorpe.com>
2023-08-20 10:39:09 -0700

    remove fish config

--- config/fish/config.fish
+++
@@ -1,9 +0,0 @@
-if status is-interactive
-    # Commands to run in interactive sessions can go here
-    alias cat=bat
-    alias vi='nvim'
-    alias vim='nvim'
-
-    source ~/.asdf/asdf.fish
-end
-# test -e /Users/rob/.iterm2_shell_integration.fish ; and source /Users/rob/.iterm2_shell_integration.fish ; or true
