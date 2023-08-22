70508ec7eb7104d3a0488f41918546860304be51
Rob Rainthorpe <robr@robrainthorpe.com>
2023-08-21 17:41:05 -0700

    prep for multi-os setup

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -10,8 +10,8 @@ end
 
 # >>> conda initialize >>>
 # !! Contents within this block are managed by 'conda init' !!
-if test -f /Users/rob/miniconda3/bin/conda
-    eval /Users/rob/miniconda3/bin/conda "shell.fish" "hook" $argv | source
+if test -f ~/miniconda3/bin/conda
+    eval ~/miniconda3/bin/conda "shell.fish" "hook" $argv | source
 end
 # <<< conda initialize <<<
 
