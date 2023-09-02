ca4dbd662612d1b36bb55f2aa51ff0f89a146b66
Rob Rainthorpe <robr@robrainthorpe.com>
2023-09-02 15:03:08 -0700

    lsp rejig complete

--- config/nvim/lua/config/keymaps.lua
+++ config/nvim/lua/config/keymaps.lua
@@ -26,7 +26,7 @@ vim.api.nvim_create_autocmd('TextYankPost', {
 })
 
 
--- Diagnostic keymaps
+-- Diagnostic keymaps - LSP uses these
 vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, { desc = 'Go to previous diagnostic message' })
 vim.keymap.set('n', ']d', vim.diagnostic.goto_next, { desc = 'Go to next diagnostic message' })
 vim.keymap.set('n', '<leader>e', vim.diagnostic.open_float, { desc = 'Open floating diagnostic message' })
