0d47510c719401c5b0c3e3fad652c4222b250c10
Rob Rainthorpe <robr@robrainthorpe.com>
2023-09-02 10:54:19 -0700

    neotree tweaks

--- config/nvim/lua/plugins/neo-tree.lua
+++ config/nvim/lua/plugins/neo-tree.lua
@@ -1,14 +1,49 @@
 vim.cmd([[ let g:neo_tree_remove_legacy_commands = 1 ]])
 
-vim.keymap.set('n', '<c-n>', ':Neotree toggle<CR>')
-
 return {
   "nvim-neo-tree/neo-tree.nvim",
   version = "*",
+  cmd = 'Neotree',
   dependencies = {
     "nvim-lua/plenary.nvim",
     "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
     "MunifTanjim/nui.nvim",
+  },
+  keys = {
+    {
+      "<leader>fe",
+      function()
+        require("neo-tree.command").execute({ toggle = true, dir = vim.loop.cwd() })
+      end,
+      desc = "Explorer NeoTree (cwd)",
+    },
+    { "<leader>e", "<leader>fe", desc = "Explorer NeoTree (cwd)", remap = true },
+    { "<c-n>",     "<leader>fe", desc = "Explorer NeoTree (cwd)", remap = true },
+  },
+  deactivate = function()
+    vim.cmd([[Neotree close]])
+  end,
+  opts = {
+    sources = { "filesystem", "buffers", "git_status", "document_symbols" },
+    open_files_do_not_replace_types = { "terminal", "Trouble", "qf", "Outline" },
+    filesystem = {
+      bind_to_cwd = false,
+      follow_current_file = { enabled = true },
+      use_libuv_file_watcher = true,
+    },
+    window = {
+      mappings = {
+        ["<space>"] = "none",
+      },
+    },
+    default_component_configs = {
+      indent = {
+        with_expanders = true, -- if nil and file nesting is enabled, will enable expanders
+        expander_collapsed = "",
+        expander_expanded = "",
+        expander_highlight = "NeoTreeExpander",
+      },
+    },
   },
   config = function()
     require('neo-tree').setup {}
