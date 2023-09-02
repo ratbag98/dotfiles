c79e80c5be5784c44de92e4d32ceb600af66cb76
Rob Rainthorpe <robr@robrainthorpe.com>
2023-09-02 10:40:34 -0700

    more nvim tidying

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -1,12 +1,11 @@
 -- ripgrep and fd will definitely need installing before this will work
 
 require('config.keymaps')
-require('config.bootstrap_lazy')
-require('lazy').setup("plugins")
+require('config.lazy')
 require('config.options')
+require('plugins.telescope.mappings')
 
 require('config.lspconfig')
-require('config.telescope')
 require('config.cmp_luasnip')
 
 require('config.colorscheme')
