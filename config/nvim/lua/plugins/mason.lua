cbf7743504f582c1bcbe22fc97be78e63783bdbc
Rob Rainthorpe <robr@robrainthorpe.com>
2023-09-29 16:56:39 -0700

    added mypy type checker

--- config/nvim/lua/plugins/mason.lua
+++ config/nvim/lua/plugins/mason.lua
@@ -12,7 +12,9 @@ return {
     ensure_installed = {
       "pyright",  -- LSP for python
       "ruff-lsp", -- linter for python (includes flake8, pep8, etc.)
+      "mypy",
       "debugpy",  -- debugger
+      'taplo',
       "black",    -- formatter
       "isort",    -- organize imports
       "taplo",    -- LSP for toml (for pyproject.toml files)
@@ -27,8 +29,6 @@ return {
       'prismals',
       'elixirls',
       'rust_analyzer',
-      'taplo',
-      'pyright',
 
     },
   },
