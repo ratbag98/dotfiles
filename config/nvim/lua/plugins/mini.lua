19d90810c4970b4d11520c31c08aff1357927a3e
Rob Rainthorpe <robr@robrainthorpe.com>
2023-10-18 20:27:00 -0700

    messing with mini

--- config/nvim/lua/plugins/mini.lua
+++ config/nvim/lua/plugins/mini.lua
@@ -1,17 +1,22 @@
-vim.api.nvim_set_keymap('n', '<c-n>', ":lua MiniFiles.open()<CR>", { silent = true, noremap = true })
+vim.api.nvim_set_keymap("n", "<c-n>", ":lua MiniFiles.open()<CR>", { silent = true, noremap = true })
 return {
   {
     "echasnovski/mini.nvim",
     config = function()
-      require('mini.pairs').setup {}
-      require('mini.surround').setup {}
-      require("mini.comment").setup {}
-      require("mini.files").setup {
+      require("mini.pairs").setup({})
+      require("mini.surround").setup({})
+      require("mini.comment").setup({})
+      require("mini.files").setup({
         windows = {
           preview = true,
-          width_preview = 30
-        }
-      }
-    end
+          width_preview = 30,
+        },
+      })
+      require("mini.animate").setup({
+        scroll = {
+          enable = false,
+        },
+      })
+    end,
   },
 }
