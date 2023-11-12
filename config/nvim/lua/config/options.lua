6df27a8d17e131dab2c758a264fcba125ecd75d4
Rob Rainthorpe <robr@robrainthorpe.com>
2023-11-12 15:53:02 -0800

    more plugin cleaning

--- config/nvim/lua/config/options.lua
+++ config/nvim/lua/config/options.lua
@@ -49,12 +49,22 @@ vim.wo.foldenable = false
 vim.wo.foldlevel = 2
 
 vim.diagnostic.config({
-  virtual_text = false,
+  virtual_text = true,
   float = {
     header = false,
     border = "rounded",
     focusable = true,
   },
+  signs = true,
+  underline = true,
+  update_in_insert = true,
+  severity_sort = true,
 })
+
+local signs = { Error = "󰅚 ", Warn = "󰀪 ", Hint = "󰌶 ", Info = " " }
+for type, icon in pairs(signs) do
+  local hl = "DiagnosticSign" .. type
+  vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = hl })
+end
 
 vim.cmd([[ let g:loaded_perl_provider = 0 ]])
--- config/nvim/lua/plugins/init.lua
+++ config/nvim/lua/plugins/init.lua
@@ -2,19 +2,27 @@
 -- more complex setup should be in a separate file per plugin
 return {
   -- Git related plugins
-  'tpope/vim-fugitive',
-  'tpope/vim-rhubarb',
+  "tpope/vim-fugitive",
+  "tpope/vim-rhubarb",
 
   -- create a session file, suitable for tmux resurrection
-  'tpope/vim-obsession',
+  "tpope/vim-obsession",
 
   -- Detect tabstop and shiftwidth automatically
-  'tpope/vim-sleuth',
+  "tpope/vim-sleuth",
 
   -- Useful plugin to show you pending keybinds.
-  { 'folke/which-key.nvim',   opts = {} },
+  {
+    "folke/which-key.nvim",
+    event = "VeryLazy",
+    init = function()
+      vim.o.timeout = true
+      vim.o.timeoutlen = 300
+    end,
+    opts = { window = { border = "single" } },
+  },
 
-  { 'mbbill/undotree' },
-  { "stevearc/dressing.nvim", event = "VeryLazy" }
+  { "mbbill/undotree" },
+  { "stevearc/dressing.nvim", event = "VeryLazy" },
 }
 -- vim: ts=2 sts=2 sw=2 et
